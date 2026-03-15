7fe2ab344a898a7ea9c0b52d3f86afe439caaacd
buhron <mbh90180@gmail.com>
2026-03-15 17:40:21 -0400

    Delete android/app/src/main/kotlin directory

--- android/app/src/main/kotlin/com/buhron/epiclauncher/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.buhron.epiclauncher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/epiclauncher/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.epiclauncher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
